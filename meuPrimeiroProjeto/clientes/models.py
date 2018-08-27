eb67714390086145ccb8ce996a05c6c26fc761be
Edson <edson.rodgon@gmail.com>
2018-08-27 10:54:21 -0300

    Terceiro commit

--- meuPrimeiroProjeto/clientes/models.py
+++ meuPrimeiroProjeto/clientes/models.py
@@ -5,7 +5,7 @@ class Person(models.Model):
     last_name = models.CharField(max_length=30)
     age = models.DecimalField(max_digits=5, decimal_places=2)
     bio = models.TextField()
-    phone = models.ImageField(upload_to='clients_photos', null= True, blank= True)
+    photo = models.ImageField(upload_to='clients_photos', null= True, blank= True)
 
     def __str__(self):
         return self.first_name + ' ' + self.last_name
